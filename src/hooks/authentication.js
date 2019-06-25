21a144eac0ca47b8653332c1f4253a49ba7f00e7
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-06-25 15:28:04 +0700

    updates authentication global hook

--- src/hooks/authentication.js
+++ src/hooks/authentication.js
@@ -1,4 +1,20 @@
-export default (req, res, next) => {
-  // console.log('authentication >>> requested-data:', req)
+'use strict'
+
+import config from 'config'
+import jwt from 'jsonwebtoken'
+
+import CustomError from '../helpers/custom-error'
+
+export default async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send(new CustomError('401', 'Missing credential').toJson())
+  }
+
+  try {
+    await jwt.verify(req.headers.authorization, config.auth.secret)
+  } catch (err) {
+    return res.status(401).send(new CustomError('401', 'Invalid credential').toJson())
+  }
+
   next()
 }
