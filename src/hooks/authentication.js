0bdcb580bb627b125ebea7945ddaf5452c6f61ce
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-06-28 11:37:11 +0700

    updates authentication global hook method

--- src/hooks/authentication.js
+++ src/hooks/authentication.js
@@ -1,24 +1,10 @@
 'use strict'
 
-import config from 'config'
-import jwt from 'jsonwebtoken'
-
+import CommonHelper from '../helpers/common'
 import CustomError from '../helpers/custom-error'
-import Models from '../models'
 
 export default async (req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(401).send(new CustomError('401', 'Missing credential').toJSON())
-  }
-
-  try {
-    const { userId, exp } = jwt.verify(req.headers.authorization, config.auth.secret)
-    const { dataValues: user } = Models.getModels().users.get(userId)
-
-    if (!user || (exp > Date.now())) {
-      throw new Error()
-    }
-  } catch (err) {
+  if (!req.headers.authorization || !CommonHelper.verifyToken(req.headers.authorization)) {
     return res.status(401).send(new CustomError('401', 'Invalid credential').toJSON())
   }
 
