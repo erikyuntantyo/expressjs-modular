4188340e4b451fabf0c4fc7d0fe1bfaa9237f60a
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-06-30 22:49:18 +0700

    updates service global hook authentication method

--- src/hooks/authentication.js
+++ src/hooks/authentication.js
@@ -4,8 +4,10 @@ import CommonHelper from '../helpers/common'
 import CustomError from '../helpers/custom-error'
 
 export default async (req, res, next) => {
-  if (!req.headers.authorization || !CommonHelper.verifyToken(req.headers.authorization)) {
-    return res.status(401).send(new CustomError('401', 'Invalid credential').toJSON())
+  try {
+    await CommonHelper.verifyAuthToken(req.headers.authorization)
+  } catch (error) {
+    return res.status(error.statusCode || 401).send(new CustomError(error.statusCode || 401, error.message).toJSON())
   }
 
   next()
