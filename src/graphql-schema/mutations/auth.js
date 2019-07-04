9c4e9bd2ba6c3182659e8f7829400eccad7e7922
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-07-04 09:38:39 +0700

    adds graphql reset token method

--- src/graphql-schema/mutations/auth.js
+++ src/graphql-schema/mutations/auth.js
@@ -2,23 +2,23 @@
 
 import bcrypt from 'bcrypt'
 import config from 'config'
-import { GraphQLString } from 'graphql'
+import { GraphQLError, GraphQLNonNull, GraphQLString } from 'graphql'
 import jwt from 'jsonwebtoken'
 import uuidv4 from 'uuidv4'
 
 import { GraphQLAuth } from '../types'
 import Models from '../../models'
-import { GraphQLInvalidCredentialError } from '../../helpers/graphql-custom-errors'
+import { GraphQLInternalServerError, GraphQLInvalidCredentialError } from '../../helpers/graphql-custom-errors'
 
 export default {
   authToken: {
     type: GraphQLAuth,
     args: {
       username: {
-        type: GraphQLString
+        type: new GraphQLNonNull(GraphQLString)
       },
       password: {
-        type: GraphQLString
+        type: new GraphQLNonNull(GraphQLString)
       }
     },
     resolve: async (rootValue, { username, password }) => {
@@ -27,19 +27,19 @@ export default {
       })
 
       if (total > 0) {
-        const [user] = data
+        const [{ id, password: _password, type }] = data
 
-        if (await bcrypt.compare(password, user.password)) {
+        if (await bcrypt.compare(password, _password)) {
           const now = Date.now()
           const exp = now + (3600 * 1000)
-          const resetKey = await bcrypt.hash(`${username}:${password}`, 10)
+          const resetKey = await bcrypt.hash(`${username}:${_password}`, 10)
           const token = jwt.sign({
-            userId: user.id,
-            type: user.type,
+            userId: id,
+            type: type,
             iat: now,
             exp,
             iss: 'expressjs',
-            sub: 'anonymous',
+            sub: 'graphql',
             jwtid: uuidv4()
           }, config.auth.secret)
 
@@ -50,5 +50,43 @@ export default {
       throw new GraphQLInvalidCredentialError()
     }
   },
-  // resetToken: {}
+  resetToken: {
+    type: GraphQLAuth,
+    args: {
+      resetKey: {
+        type: new GraphQLNonNull(GraphQLString)
+      }
+    },
+    resolve: async (rootValue, { resetKey }, { headers: { authorization } }) => {
+      try {
+        const { userId } = jwt.verify(authorization, config.auth.secret)
+        const { id, username, password, type } = Models.getModels().users.get(userId)
+
+        if (await bcrypt.compare(`${username}:${password}`, resetKey)) {
+          const now = Date.now()
+          const exp = now + (3600 * 1000)
+          const resetKey = await bcrypt.hash(`${username}:${password}`, 10)
+          const token = jwt.sign({
+            userId: id,
+            type: type,
+            iat: now,
+            exp,
+            iss: 'expressjs',
+            sub: 'graphql',
+            jwtid: uuidv4()
+          }, config.auth.secret)
+
+          return { token, resetKey }
+        }
+
+        throw new GraphQLInvalidCredentialError('Invalid reset key.')
+      } catch (error) {
+        if (error instanceof GraphQLError) {
+          throw error
+        }
+
+        throw new GraphQLInternalServerError(error)
+      }
+    }
+  }
 }
