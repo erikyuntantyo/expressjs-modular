09dd1adca0fd8afc5516adbea50e9775250502a5
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-07-04 09:41:03 +0700

    adds graphql create, update, and delete account methods

--- src/graphql-schema/mutations/accounts.js
+++ src/graphql-schema/mutations/accounts.js
@@ -1,7 +1,142 @@
 'use strict'
 
+import bcrypt from 'bcrypt'
+import { GraphQLBoolean, GraphQLID, GraphQLNonNull, GraphQLString } from 'graphql'
+
+import CommonHelper from '../../helpers/common'
+import { GraphQLAccount, GraphQLDate } from '../types'
+import Models from '../../models'
+
 export default {
-  createAccount: {},
-  disableAccount: {},
-  updateAccount: {}
+  createAccount: {
+    type: GraphQLAccount,
+    args: {
+      username: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      email: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      password: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      accountNumber: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      bankId: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      firstName: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      lastName: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      dob: {
+        type: new GraphQLNonNull(GraphQLDate)
+      },
+      address: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      phone: {
+        type: new GraphQLNonNull(GraphQLString)
+      }
+    },
+    resolve: async (rootValue, {
+      username,
+      email,
+      password,
+      accountNumber,
+      bankId,
+      firstName,
+      lastName,
+      dob,
+      address,
+      phone
+    }, { headers: { authorization } }) => {
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
+
+        const createdAccount = await Models.getModels().accounts.create({
+          accountNumber,
+          bankId,
+          firstName,
+          lastName,
+          dob,
+          address,
+          phone
+        })
+
+        await Models.getModels().users.create({
+          username,
+          email,
+          password: await bcrypt.hash(password, 10),
+          accountId: createdAccount.id
+        })
+
+        return createdAccount
+      } catch (err) {
+        throw err
+      }
+    }
+  },
+  disableAccount: {
+    type: GraphQLAccount,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID)
+      },
+      disabled: {
+        type: GraphQLBoolean
+      }
+    },
+    resolve: async (rootValue, { id, disabled = true }, { headers: { authorization } }) => {
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
+        return await Models.getModels().accounts.update(id, { disabled })
+      } catch (err) {
+        throw err
+      }
+    }
+  },
+  updateAccount: {
+    type: GraphQLAccount,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID)
+      },
+      email: {
+        type: GraphQLString
+      },
+      firstName: {
+        type: GraphQLString
+      },
+      lastName: {
+        type: GraphQLString
+      },
+      dob: {
+        type: GraphQLDate
+      },
+      address: {
+        type: GraphQLString
+      },
+      phone: {
+        type: GraphQLString
+      }
+    },
+    resolve: async (rootValue, { id, email, ...data }, { headers: { authorization } }) => {
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
+
+        if (email) {
+          const { data: [{ id: userId }] } = await Models.getModels().users.find({ where: { accountId: id } })
+          await Models.getModels().users.update(userId, { email })
+        }
+
+        return await Models.getModels().accounts.update(id, data)
+      } catch (err) {
+        throw err
+      }
+    }
+  }
 }
