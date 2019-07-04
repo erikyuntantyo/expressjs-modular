f5c915dcd86bf8623874aaa5140b15d0417803e1
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-07-04 09:41:38 +0700

    updates graphql users methods

--- src/graphql-schema/mutations/users.js
+++ src/graphql-schema/mutations/users.js
@@ -1,59 +1,93 @@
 'use strict'
 
 import bcrypt from 'bcrypt'
-import { GraphQLID, GraphQLString } from 'graphql'
+import { GraphQLBoolean, GraphQLID, GraphQLNonNull, GraphQLString } from 'graphql'
 import uuidv4 from 'uuidv4'
 
 import CommonHelper from '../../helpers/common'
-import { GraphQLUser } from '../types'
+import { GraphQLMethodNotAllowedError, GraphQLInvalidCredentialError } from '../../helpers/graphql-custom-errors'
+import { GraphQLUser, GraphQLUserRoles } from '../types'
 import Models from '../../models'
 
 export default {
-  createUser: {
+  createAdmin: {
     type: GraphQLUser,
     args: {
       username: {
-        type: GraphQLString
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      email: {
+        type: new GraphQLNonNull(GraphQLString)
       },
       password: {
-        type: GraphQLString
+        type: new GraphQLNonNull(GraphQLString)
       },
-      type: {
-        type: GraphQLString
+      role: {
+        type: new GraphQLNonNull(GraphQLUserRoles)
       }
     },
-    resolve: async (rootValue, data) => {
-      data.id = uuidv4()
-      data.password = await bcrypt.hash(data.password, 10)
+    resolve: async (rootValue, data, { headers: { authorization } }) => {
+      // NB: add email verification to make sure the user is currently available
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
 
-      return await Models.getModels().users.create(data)
+        data.id = uuidv4()
+        data.password = await bcrypt.hash(data.password, 10)
+
+        if (data.role === 'customer') {
+          throw new GraphQLMethodNotAllowedError('Cannot create customer using this method. Please use "createCustomer" method instead.')
+        }
+
+        return await Models.getModels().users.create(data)
+      } catch (err) {
+        throw err
+      }
     }
   },
-  // disableUser: {},
-  updateUser: {
+  disableUser: {
     type: GraphQLUser,
     args: {
       id: {
-        type: GraphQLID
+        type: new GraphQLNonNull(GraphQLID)
       },
-      username: {
-        type: GraphQLString
-      },
-      password: {
-        type: GraphQLString
-      },
-      type: {
-        type: GraphQLString
+      disabled: {
+        type: GraphQLBoolean
       }
     },
-    resolve: async (rootValue, { id, username, password, type }, { headers: { authorization } }) => {
+    resolve: async (rootValue, { id, disabled = true }, { headers: { authorization } }) => {
       try {
         await CommonHelper.verifyAuthToken(authorization)
-        return await Models.getModels().users.update(id, {
-          username,
-          password: await bcrypt.hash(password, 10),
-          type
-        })
+        return await Models.getModels().users.update(id, { disabled })
+      } catch (err) {
+        throw err
+      }
+    }
+  },
+  resetPassword: {
+    type: GraphQLUser,
+    args: {
+      username: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      oldPassword: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      newPassword: {
+        type: new GraphQLNonNull(GraphQLString)
+      }
+    },
+    resolve: async (rootValue, { username, oldPassword, newPassword }, { headers: { authorization } }) => {
+      // NB: add email verification to make it more secure
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
+
+        const { data: [{ id, password }] } = await Models.getModels().users.find({ where: { username } })
+
+        if (password && await bcrypt.compare(oldPassword, password)) {
+          return await Models.getModels().users.update(id, { password: await bcrypt.hash(newPassword) })
+        } else {
+          throw new GraphQLInvalidCredentialError()
+        }
       } catch (err) {
         throw err
       }
