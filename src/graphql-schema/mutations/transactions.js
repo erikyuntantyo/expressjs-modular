f6aa8a9b59b74da46ea1860e036f6c746ad16e15
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-07-04 09:49:11 +0700

    updates graphql transaction mutation methods

--- src/graphql-schema/mutations/transactions.js
+++ src/graphql-schema/mutations/transactions.js
@@ -1,7 +1,150 @@
 'use strict'
 
+import { GraphQLFloat, GraphQLID, GraphQLNonNull, GraphQLString } from 'graphql'
+
+import CommonHelper from '../../helpers/common'
+import { GraphQLMethodNotAllowedError } from '../../helpers/graphql-custom-errors'
+import { GraphQLTransaction } from '../types'
+import Models from '../../models'
+
 export default {
-  deposit: {},
-  transfer: {},
-  withdraw: {}
+  deposit: {
+    type: GraphQLTransaction,
+    args: {
+      userId: {
+        type: new GraphQLNonNull(GraphQLID)
+      },
+      amount: {
+        type: new GraphQLNonNull(GraphQLFloat)
+      }
+    },
+    resolve: async (rootValue, data, { headers: { authorization } }) => {
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
+
+        const { data: [{ balance }] } = await Models.getModels().transactions.find({
+          where: {
+            userId: data.userId
+          },
+          order: [['createdAt', 'DESC']]
+        }) || {}
+
+        data.balance = (balance || 0) + data.amount
+        data.remarks = `DEP ${data.amount}`
+
+        return await Models.getModels().transactions.create({
+          ...data,
+          type: 'CR'
+        })
+      } catch (err) {
+        throw err
+      }
+    }
+  },
+  transfer: {
+    type: GraphQLTransaction,
+    args: {
+      userId: {
+        type: new GraphQLNonNull(GraphQLID)
+      },
+      destUserId: {
+        type: new GraphQLNonNull(GraphQLID)
+      },
+      amount: {
+        type: new GraphQLNonNull(GraphQLFloat)
+      },
+      description: {
+        type: GraphQLString
+      }
+    },
+    resolve: async (rootValue, { userId, destUserId, amount, description }, { headers: { authorization } }) => {
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
+
+        const { data: [{ balance: destBalance }] } = await Models.getModels().transactions.find({
+          where: {
+            userId: destUserId
+          },
+          order: [['createdAt', 'DESC']]
+        }) || {}
+
+        const { data: [{ balance }] } = await Models.getModels().transactions.find({
+          where: { userId },
+          order: [['createdAt', 'DESC']]
+        }) || {}
+
+        if (amount > balance) {
+          throw new GraphQLMethodNotAllowedError('Transfers may not be made because the balance is lower than expected.')
+        }
+
+        const { firstName: destFirstName, lastName: destLastName } = await Models.getModels().users.get(destUserId)
+
+        if (!destFirstName || !destLastName) {
+          throw new GraphQLMethodNotAllowedError('Unknown destination account.')
+        }
+
+        const { firstName, lastName } = await Models.getModels().users.get(userId)
+
+        await Models.getModels().transactions.create({
+          userId: destUserId,
+          amount: amount,
+          description,
+          balance: (destBalance + amount),
+          remarks: `TRF DB ${amount}\n${firstName} ${lastName}`,
+          type: 'TRF'
+        })
+
+        return await Models.getModels().transactions.create({
+          userId,
+          amount,
+          description,
+          balance: balance - amount,
+          remarks: `TRF CR ${amount}\n${destFirstName} ${destLastName}`,
+          type: 'TRF'
+        })
+      } catch (err) {
+        throw err
+      }
+    }
+  },
+  withdraw: {
+    type: GraphQLTransaction,
+    args: {
+      userId: {
+        type: new GraphQLNonNull(GraphQLID)
+      },
+      amount: {
+        type: new GraphQLNonNull(GraphQLFloat)
+      },
+      description: {
+        type: new GraphQLNonNull(GraphQLString)
+      }
+    },
+    resolve: async (rootValue, data, { headers: { authorization } }) => {
+      try {
+        await CommonHelper.verifyAuthToken(authorization)
+
+        const { data: [{ balance }] } = await Models.getModels().transactions.find({
+          where: {
+            userId: data.userId
+          },
+          order: [['createdAt', 'DESC']]
+        }) || {}
+
+        if (balance < data.amount) {
+          throw new GraphQLMethodNotAllowedError('Withdrawals may not be made because the balance is lower than expected.')
+        }
+
+        data.balance = (balance || 0) + data.amount
+        data.remarks = `WTDR ${data.amount}`
+
+        return await Models.getModels().transactions.create({
+          ...data,
+          type: 'DB'
+        })
+      } catch (err) {
+        throw err
+      }
+    }
+  }
 }
