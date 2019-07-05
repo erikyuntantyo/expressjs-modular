b6509522d8e095c2a7e30b8530ed5a8dc107d3ce
Erik Yuntantyo <erik.yuntantyo@gmail.com>
2019-07-05 08:16:05 +0700

    updates graphql schema mutations index file

--- src/graphql-schema/mutations/index.js
+++ src/graphql-schema/mutations/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+export { default as GraphQLAccountMutations } from './accounts'
 export { default as GraphQLAuthMutations } from './auth'
 export { default as GraphQLBankMutations } from './banks'
+export { default as GraphQLTransactionMutations } from './transactions'
 export { default as GraphQLUserMutations } from './users'
